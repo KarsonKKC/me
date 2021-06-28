5791912a72964d41348c615955166eab7ab52a16
Karson Chow <karsonchow2@hotmail.com>
2021-06-28 11:45:25 +1000

    Exercise 3 final

--- set3/exercise3.py
+++ set3/exercise3.py
@@ -5,16 +5,41 @@ Steps on the way to making your own guessing game.
 
 import random
 
-def super_asker(low, high):
+def not_number_rejector_1(message):
+    """Ask for a number repeatedly until actually given one.
+
+    Ask for a number, and if the response is actually NOT a number
+    (e.g. "cow", "six", "8!") then throw it out and ask for an actual number.
+    When you do get a number, return it.
+    """
     while True:
         try:
-            x = int(input("Please input a number"))
+            x = int(input("Enter a low number: "))
+        except Exception as e:
+            print(f"Please try again {e}")
+            continue
+        else:
+            return x
+
+def super_asker(low, high):
+    """Robust asking function.
+
+    Combine what you learnt from stubborn_asker and not_number_rejector
+    to make a function that does it all!
+    Try to call at least one of the other functions to minimise the
+    amount of code.
+    """
+
+    while True:
+        try:
+            x = int(input("Now, Enter a high number: "))
             if low <= x <= high:
                 return x
             else:
                 print("Please try again")
         except Exception as e:
             print(f"Please try again {e}")
+            
 
 def advancedGuessingGame():
     """Play a guessing game with a user.
@@ -37,9 +62,24 @@ def advancedGuessingGame():
     """
     print("\nWelcome to the guessing game!")
     print("This game will ask for two numbers, What number is in your mind?")
-    high = super_asker(0, 100)
-    low = super_asker(0, high)
+    low_number = not_number_rejector_1("Enter a low number: ")
+    high_number = super_asker(low_number, float('inf'))
+    print(f"OK then, a number between {low_number} and {high_number} ?")
+
+    actualNumber = random.randint(low_number, high_number)
     
+    guessed = False
+
+    while not guessed:
+        guessed_number = int(input("Guess a number: "))
+        print(f"You guessed {guessed_number},")
+        if guessed_number == actualNumber:
+            print(f"You got it!! It was {actualNumber}!")
+            guessed = True
+        elif guessed_number < actualNumber:
+            print("Too small, try again!")
+        else:
+            print("Too big, try again!")
     return "You got it!"
     # the tests are looking for the exact string "You got it!". Don't modify that!
 
--- set3/readme.md
+++ set3/readme.md
@@ -1,1 +1,8 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+This week was confusing and tough, My head was every where and i could'nt figure out most of the commands.
+But I've looked at it again and realised how the function works.
+I copied both Super_asker and Not number rejecter to put it in the guessing game. It was fun to think how it is working.
+
+I've also learnt how to use float('inf'), it means to find the infinity value in Python 3. It will allow the highest number. If you want it to be negative, just put - in front of inf.
+
+This week is mainly about while loops and try, except. 
